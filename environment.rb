ec6f99dcd7e4ec87dd52e84fff23d07da6a786de
mguachun <53132301+mguachun@users.noreply.github.com>
2021-02-14 20:59:17 -0500

    Delete environment.rb

--- environment.rb
+++
@@ -1,6 +0,0 @@
-
-#/Users/melissaguachun/upcoming_films/lib/upcoming_films/upcoming_films.rb
-require_relative "../lib/upcoming_films/cli.rb"
-require_relative "../lib/upcoming_films/upcoming_films.rb"
-require_relative "../lib/upcoming_films/scraper.rb"
-#require_relative "../lib/version.rb"
